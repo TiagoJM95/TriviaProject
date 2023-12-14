59d4a3f225757f49a6b3284d3b75b5f5a378eac9
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2023-12-14 19:52:48 +0000

    Server Class Done, Game Class Ongoing

--- ProjectTrivia/src/ServerClient/Server/Server/Server.java
+++ ProjectTrivia/src/ServerClient/Server/Server/Server.java
@@ -1,119 +1,44 @@
 package ServerClient.Server.Server;
 
-import ServerClient.Server.Commands.Command;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import Game.Game.Game;
 
 public class Server {
     private ServerSocket serverSocket;
     private ExecutorService service;
-    private final List<PlayerHandler> players;
-
-    public Server(){
-        players = new CopyOnWriteArrayList<>();
-    }
+    private Game game;
 
     public void start() throws IOException {
-        serverSocket = new ServerSocket(8080);
-        service = Executors.newCachedThreadPool();
-        acceptConnection();
+       serverSocket = new ServerSocket(8080);
+       service = Executors.newCachedThreadPool();
+       System.out.println("Server started at port 8080");
+
+       while(!serverSocket.isClosed()){
+
+           if(!gameCreated()){
+              createGame();
+           }
+
+           if(!gameHasStarted()){
+               game.acceptPlayer(serverSocket.accept());
+           }
+
+       }
     }
 
-    private void acceptConnection() throws IOException {
-        Socket playerSocket = serverSocket.accept();
-        PlayerHandler playerHandler = new PlayerHandler(playerSocket);
-        service.submit(playerHandler);
+    private boolean gameHasStarted() {
+        return game.isGameStarted();
     }
 
-    private void addPlayer(PlayerHandler playerHandler){
-        players.add(playerHandler);
-        //Mensagem de boas vindas;
-        //Regras do jogo;
-        //Pedir nome ao jogador;
+    private void createGame() {
+        this.game = new Game(this);
+        service.execute(game);
     }
 
-    public void remove(PlayerHandler playerHandler){
-        players.remove(playerHandler);
-    }
-
-    public void broadcast(String string){
-        players.forEach(player -> player.send(string));
-    }
-
-
-
-   public class PlayerHandler implements Runnable{
-        private String name;
-        private final Socket playerSocket;
-        private final BufferedReader in;
-        private final PrintWriter out;
-        private String input;
-
-
-        public PlayerHandler(Socket playerSocket) throws IOException {
-            this.playerSocket = playerSocket;
-            in = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
-            out = new PrintWriter(playerSocket.getOutputStream(), true);
-        }
-
-        @Override
-        public void run() {
-            addPlayer(this);
-            while(!playerSocket.isClosed()){
-                try {
-                    input = in.readLine();
-                    if(isCommand(input)){
-                        command(input);
-                        continue;
-                    }
-                    //you need to input commands;
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-
-        private boolean isCommand(String input){
-            return input.startsWith("/");
-        }
-
-        private void command(String input){
-            String commandDescription = input.split(" ")[0];
-            Command command = Command.getCommand(commandDescription);
-
-            command.getHandler().execute(Server.this, this);
-        }
-
-        public void send(String input){
-            out.println(input);
-        }
-
-        public void close(){
-            try {
-                playerSocket.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public String getName(){
-            return name;
-        }
-
-        public void setName(String name){
-            this.name = name;
-        }
-
-        public String getInput() {
-            return input;
-        }
+    private boolean gameCreated() {
+        return game != null;
     }
 }
--- ProjectTrivia/src/ServerClient/Server/Server/ServerLauncher.java
+++ ProjectTrivia/src/ServerClient/Server/Server/ServerLauncher.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 public class ServerLauncher {
     public static void main(String[] args) {
         Server server = new Server();
-
         try {
             server.start();
         } catch (IOException e) {
