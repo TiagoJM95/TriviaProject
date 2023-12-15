0868d483410132a56b74f0f8466f61a3f4aa4219
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2023-12-15 12:08:47 +0000

    Player Class updated

--- ProjectTrivia/src/ServerClient/Player/Player.java
+++ ProjectTrivia/src/ServerClient/Player/Player.java
@@ -8,50 +8,55 @@ import java.net.Socket;
 
 public class Player {
     private Socket playerSocket;
-    private boolean myTurn;
+    BufferedReader consoleIn;
+    BufferedReader serverIn;
+    PrintWriter serverOut;
+
 
     public void start(String host, int port) throws IOException {
-        playerSocket = new Socket(host, port);
-        BufferedReader serverIn = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
-        PrintWriter serverOut = new PrintWriter(playerSocket.getOutputStream(), true);
-        BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
+        initialize(host, port);
+        Thread thread = new Thread(new Listener(serverIn));
+        thread.start();
+        sendMessages();
+    }
 
-        Thread listenerThread = new Thread(new Listener(serverIn));
-        listenerThread.start();
+    private void sendMessages() throws IOException {
+        String messageToServer;
 
-        String messageToServer = "";
-        while(!messageToServer.equals("/exit")) {
+        while(!playerSocket.isClosed()){
+
             messageToServer = consoleIn.readLine();
             serverOut.println(messageToServer);
-        }
 
-        try {
-            listenerThread.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
+    }
 
-        serverIn.close();
-        serverOut.close();
-        consoleIn.close();
-        playerSocket.close();
+    private void initialize(String host, int port) throws IOException {
+
+        playerSocket = new Socket(host, port);
+        consoleIn = new BufferedReader(new InputStreamReader(System.in));
+        serverIn = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
+        serverOut = new PrintWriter(playerSocket.getOutputStream(), true);
+
     }
 
     static class Listener implements Runnable{
         private final BufferedReader in;
 
-        Listener(BufferedReader in){
-            this.in = in;
+        public Listener(BufferedReader serverIn){
+            this.in = serverIn;
         }
 
         @Override
         public void run() {
-            String messageFromServer = null;
-            try {
-                while (!(messageFromServer = in.readLine()).equals("exit")) {
+
+            String messageFromServer;
+
+            try{
+                while(!(messageFromServer = in.readLine()).equals("disconnect")){
                     System.out.println(messageFromServer);
                 }
-            } catch (IOException e) {
+            } catch (IOException e){
                 throw new RuntimeException(e);
             }
         }
