aa140af01bde33bb79e555f48865fad4312d7b02
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2023-12-16 13:10:25 +0000

    Board Andre

--- ProjectTrivia/src/Game/Board/Board.java
+++ ProjectTrivia/src/Game/Board/Board.java
@@ -1,5 +1,50 @@
 package Game.Board;
 
 public class Board {
+    private static final String xxxxxxxxx = "+-----------+";
+    private static final String x_______x = "|           |";
+    private static final String history = "| HISTORY   |";
+    private static final String geography = "| GEOGRAPHY |";
+    private static final String blank = "             ";
+
+
+
+
+    private String[] drawBoard(){
+        return new String[]{
+                xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx,
+                history + geography + history + geography + history,
+                x_______x + x_______x + x_______x + x_______x + x_______x,
+                xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx,
+                xxxxxxxxx + blank + xxxxxxxxx + blank + xxxxxxxxx,
+                history + blank +history + blank + history,
+                x_______x + blank + x_______x + blank + x_______x,
+                xxxxxxxxx + blank + xxxxxxxxx + blank + xxxxxxxxx,
+                xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx,
+                history + geography + history + geography + history,
+                x_______x + x_______x + x_______x + x_______x + x_______x,
+                xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx,
+                xxxxxxxxx + blank + xxxxxxxxx + blank + xxxxxxxxx,
+                history + blank +history + blank + history,
+                x_______x + blank + x_______x + blank + x_______x,
+                xxxxxxxxx + blank + xxxxxxxxx + blank + xxxxxxxxx,
+                xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx,
+                history + geography + history + geography + history,
+                x_______x + x_______x + x_______x + x_______x + x_______x,
+                xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx + xxxxxxxxx};
+    }
+    public void printBoard(){
+        for (String printEachLine : drawBoard()) {
+            System.out.println(printEachLine);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.printBoard();
+    }
+
+
 
 }
