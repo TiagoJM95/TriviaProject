a1886d5012c1a007d0f1f609110de2fbff83d98c
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2023-12-20 20:46:04 +0000

    ServerLauncher deleted unused imports

--- ProjectTrivia/src/ServerClient/Server/ServerLauncher.java
+++ ProjectTrivia/src/ServerClient/Server/ServerLauncher.java
@@ -1,7 +1,5 @@
 package ServerClient.Server;
 
-import ServerClient.Server.Server;
-
 public class ServerLauncher {
     public static void main(String[] args) {
         Server server = new Server();
